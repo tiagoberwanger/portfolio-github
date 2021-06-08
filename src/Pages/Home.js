92880658dddca62d429de5b8d0644842e9a98502
Tiago Berwanger <berwangertiago@gmail.com>
2021-06-08 19:15:11 -0300

    creating directories and componentization

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Components/Header';
+import Portfolio from '../Components/Portfolio';
+import Contact from '../Components/Contact';
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <Portfolio />
+            <Contact />
+        </div>
+    );
+}
+
+export default Home;
