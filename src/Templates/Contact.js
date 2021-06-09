b9533a91e93fd171e9235145e88b22dea4248a14
Tiago Berwanger <berwangertiago@gmail.com>
2021-06-09 00:01:21 -0300

    creating contact form with links and images

--- src/Templates/Contact.js
+++ src/Templates/Contact.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { NavLink, Image } from 'react-bootstrap';
 
 function Contact() {
     return (
-        <div>
-            Contact
-        </div>
+      <div>
+        <NavLink target="_blank" href="https://linkedin.com/in/tiago-berwanger">
+          <Image src="" alt="linkedin" />
+        </NavLink>
+        <p>LinkedIn</p>
+        <NavLink target="_blank" href="https://github.com/tiagoberwanger">
+          <Image  src="" alt="git" />
+        </NavLink>
+        <p>GitHub</p>
+        <NavLink href="mailto:berwangertiago@gmail.com">
+          <Image src="" alt="mail" />
+      </NavLink>
+        <p>Contato</p>
+      </div>
     );
 }
 
 export default Contact;
+
